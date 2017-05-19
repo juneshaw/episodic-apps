eea714788009027bcf650b86c39da00490242d43
June Shaw <june.shaw@me.com>
2017-05-18 23:53:07 -0600

    Episode service test done

--- episodic-shows/src/test/java/com/example/episodicepisodes/EpisodeServiceTest.java
+++ episodic-shows/src/test/java/com/example/episodicepisodes/EpisodeServiceTest.java
@@ -9,9 +9,11 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -41,5 +43,33 @@ public class EpisodeServiceTest {
         assertThat(
                 retrievedEpisode,
                 equalTo(episode1));
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void testRead() {
+        episodeService.create(episode1);
+        episodeService.create(episode2);
+        List<Episode> episodes = repository.findAll();
+        assertThat(episodes.size(), equalTo(2));
+        assertTrue(episodes.stream().anyMatch(item -> item.getShowId().equals(episode1.getShowId())));
+        assertTrue(episodes.stream().anyMatch(item -> item.getShowId().equals(episode2.getShowId())));
+        assertTrue(episodes.stream().anyMatch(item -> item.getSeasonNumber().equals(episode1.getSeasonNumber())));
+        assertTrue(episodes.stream().anyMatch(item -> item.getSeasonNumber().equals(episode2.getSeasonNumber())));
+        assertTrue(episodes.stream().anyMatch(item -> item.getEpisodeNumber().equals(episode1.getEpisodeNumber())));
+        assertTrue(episodes.stream().anyMatch(item -> item.getEpisodeNumber().equals(episode2.getEpisodeNumber())));
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void testReadOne() {
+        Episode savedEpisode = repository.save(episode1);
+        Episode retrievedEpisode = episodeService.read(savedEpisode.getId());
+        assertThat(retrievedEpisode.getId(), equalTo(savedEpisode.getId()));
+        assertThat(retrievedEpisode.getShowId(), equalTo(savedEpisode.getShowId()));
+        assertThat(retrievedEpisode.getSeasonNumber(), equalTo(savedEpisode.getSeasonNumber()));
+        assertThat(retrievedEpisode.getEpisodeNumber(), equalTo(savedEpisode.getEpisodeNumber()));
     }
 }
