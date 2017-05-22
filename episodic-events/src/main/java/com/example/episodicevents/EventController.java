17881263d064eeace71ddd3c78ef543d41599602
June Shaw <june.shaw@me.com>
2017-05-22 14:11:56 -0600

    Added get/recent route

--- episodic-events/src/main/java/com/example/episodicevents/EventController.java
+++ episodic-events/src/main/java/com/example/episodicevents/EventController.java
@@ -1,5 +1,7 @@
 package com.example.episodicevents;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +17,14 @@ public class EventController {
     @GetMapping
     public Iterable<Event> getEvents() {
         return repository.findAll();
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Event> getRecentEvents() {
+        return repository.findAll(new PageRequest(
+                0,
+                20,
+                new Sort(Sort.Direction.DESC, "createdAt")));
     }
 
     @PostMapping
--- episodic-events/src/main/java/com/example/episodicevents/EventRepository.java
+++ episodic-events/src/main/java/com/example/episodicevents/EventRepository.java
@@ -1,6 +1,10 @@
 package com.example.episodicevents;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EventRepository extends MongoRepository<Event, Long> {
+    Page<Event> findAll(Pageable pageable);
+
 }
